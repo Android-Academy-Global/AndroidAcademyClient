5e144c6627cfa5874b5f0b143cfb6a5506bbcccd
ermolnik <ermolnik666@gmail.com>
2021-03-20 23:25:49 +0300

    add maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
   repositories {
+    mavenCentral()
     google()
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.1.2")
+    classpath("com.android.tools.build:gradle:4.1.3")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
 
     // NOTE: Do not place your application dependencies here; they belong
@@ -15,6 +16,7 @@ buildscript {
 
 allprojects {
   repositories {
+    mavenCentral()
     google()
     jcenter()
   }
