c1d6a143f6ef0710072ed292b6488de2d4598906
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-03-21 14:46:50 +0300

    App.kt - modified to use Hilt + Timber added

--- app/src/main/java/com/academy/android/App.kt
+++ app/src/main/java/com/academy/android/App.kt
@@ -1,6 +1,26 @@
 package com.academy.android
 
 import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
+@HiltAndroidApp
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+        initLogger()
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    companion object {
+        internal lateinit var INSTANCE: App
+            private set
+    }
 }
