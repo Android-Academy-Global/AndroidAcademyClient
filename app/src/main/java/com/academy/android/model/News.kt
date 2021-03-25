c749d5cd7089f75d2cd0e0fb1eaaaba96475d4a5
Aleksey <aleksey@yakovlev.spb.ru>
2021-03-25 15:38:37 +0300

    News DTO added

---
+++ app/src/main/java/com/academy/android/model/News.kt
@@ -0,0 +1,11 @@
+package com.academy.android.model
+
+import java.util.*
+
+data class News(
+    val id: Long = 0,
+    val text: String = "",
+    val link: String = "",
+    val picture: String = "",
+    val date: Date = Date()
+)
