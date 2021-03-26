c9d10192ce26f1947fa645b9003621ff01ecaeb4
Aleksey <aleksey@yakovlev.spb.ru>
2021-03-26 16:53:24 +0300

    ChatsRepository - fixed to provide correct flow of messagesCount data

--- app/src/main/java/com/academy/android/data/repositories/ChatsRepository.kt
+++ app/src/main/java/com/academy/android/data/repositories/ChatsRepository.kt
@@ -1,7 +1,13 @@
 package com.academy.android.data.repositories
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,17 +15,29 @@ import javax.inject.Singleton
 class ChatsRepository @Inject constructor(
 
 ) {
-    /**
-    * Mokk function to emulate flow of messages (buggy)
-    */
-    fun getMessagesCountForId(chatId: Long): Flow<Int> = flow {
-        var numberOfMessages = chatId.toInt()
-        repeat(10_000) {
-            kotlinx.coroutines.delay(1000)
-            val newMessages = (0..10).random()
-            if (newMessages < 5 && newMessages != 0) {
-                numberOfMessages += newMessages
-                emit(numberOfMessages)
+    init {
+        generateMessagesCount()
+    }
+
+    private val messagesCountState = MutableStateFlow<Map<Long, Int>>(emptyMap())
+
+    fun getMessagesCountForId(chatId: Long): Flow<Int> =
+        messagesCountState.filter { it.containsKey(chatId) }.map { it[chatId] ?: 0 }
+
+    private fun generateMessagesCount() {
+        val messagesCount = mutableMapOf<Long, Int>()
+        CoroutineScope(Dispatchers.IO).launch {
+            repeat(10_000) {
+                val newMessagesCount = mutableMapOf<Long, Int>()
+                (1L..8L).forEach { i ->
+                    val newMessages = (0..10).random()
+                    if (newMessages < 5 && newMessages != 0) {
+                        newMessagesCount[i] = newMessages + (messagesCount[i] ?: 0)
+                    }
+                }
+                messagesCountState.value = newMessagesCount
+                messagesCount.putAll(newMessagesCount)
+                delay(1000)
             }
         }
     }
