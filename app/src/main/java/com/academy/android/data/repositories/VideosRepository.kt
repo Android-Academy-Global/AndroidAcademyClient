ce46c8fc8c57fa01db838023348eea8bc1ea356f
paxett <y.bogdanov@gmail.com>
2021-04-01 20:22:05 +0300

    Video repository added

---
+++ app/src/main/java/com/academy/android/data/repositories/VideosRepository.kt
@@ -0,0 +1,46 @@
+package com.academy.android.data.repositories
+
+import com.academy.android.model.Videos
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+interface VideosRepositorySource {
+    val videosList: StateFlow<List<Videos>>
+}
+
+class VideosRepository @Inject constructor() : VideosRepositorySource{
+    private val _videosList = MutableStateFlow(provideMokkVideos())
+    override val videosList: StateFlow<List<Videos>> = _videosList
+
+    private fun provideMokkVideos(): List<Videos> = listOf(
+        Videos(
+            1,
+            "Android Fundamentals #0: How to Kotlin (Russian language)",
+            "27 oct. 2020",
+        "https://img.youtube.com/vi/_clrkv6VL4g/0.jpg",
+            "Moscow",
+            "Fundamentals",
+            "2020-2021"
+        ),
+        Videos(
+            2,
+            "Coroutines, Yonatan Levin - Android Developer@Monday",
+            "7 feb. 2020",
+            "https://img.youtube.com/vi/ngGI-zVJceo/0.jpg",
+            "Minsk",
+            "Advanced",
+            "2019-2020"
+        ),
+        Videos(
+            3,
+            "Android Academy TLV 2019 - Fundamentals Course - fragments by Gil Goldzweig",
+            "25 dec. 2019",
+            "https://img.youtube.com/vi/3zb7AnbFfTA/0.jpg",
+            "Tel-Aviv",
+            "Fundamentals",
+            "2019-2020"
+        )
+    )
+
+}
