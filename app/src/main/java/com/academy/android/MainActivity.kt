78ff650f371b2b328d2373897d566f1e9a7afbcd
Mikhail <mikhail.id.123@gmail.com>
2021-11-11 09:25:45 +0400

    Compose Theming

--- app/src/main/java/com/academy/android/MainActivity.kt
+++ app/src/main/java/com/academy/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.academy.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.academy.android.ui.AcademyTheme
 import com.academy.android.ui.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +13,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            AcademyTheme {
+                HomeScreen()
+            }
         }
     }
 }
