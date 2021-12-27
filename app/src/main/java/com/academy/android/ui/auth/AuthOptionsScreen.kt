4e249a4fe8c43698440a414ad0bbfc3e04dbda39
Mikhail <mikhail.id.123@gmail.com>
2021-12-27 08:31:15 +0400

    Added Academy label

--- app/src/main/java/com/academy/android/ui/auth/AuthOptionsScreen.kt
+++ app/src/main/java/com/academy/android/ui/auth/AuthOptionsScreen.kt
@@ -1,5 +1,6 @@
 package com.academy.android.ui.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.academy.android.R
 
 @Composable
@@ -20,23 +24,45 @@ fun AuthOptionsScreen(vm: AuthViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = paddingStandard, end = paddingStandard),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
+        Image(
+            painter = painterResource(id = R.drawable.top_background),
+            contentDescription = stringResource(id = R.string.top_bg_desc),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = paddingStandard),
-            onClick = { /*TODO*/ }
+        )
+        Text(
+            text = stringResource(id = R.string.auth_option_subtitle),
+            fontSize = dimensionResource(id = R.dimen.auth_options_subtitle_size).value.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(start = dimensionResource(id = R.dimen.spacing_56))
+        )
+        Text(
+            text = stringResource(id = R.string.auth_option_title),
+            fontSize = dimensionResource(id = R.dimen.auth_options_title_size).value.sp,
+            modifier = Modifier.padding(start = paddingStandard, end = paddingStandard)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = paddingStandard, end = paddingStandard),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = stringResource(id = R.string.option_login))
-        }
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = { vm.onGuestModeClick() }
-        ) {
-            Text(text = stringResource(id = R.string.option_guest))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = paddingStandard),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = stringResource(id = R.string.option_login))
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { vm.onGuestModeClick() }
+            ) {
+                Text(text = stringResource(id = R.string.option_guest))
+            }
         }
     }
 }
